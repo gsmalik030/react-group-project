561c783aa707ea7c715c69544476d07d2ff49107
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-07 11:49:15 +0200

    Added store as prop to provider

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
