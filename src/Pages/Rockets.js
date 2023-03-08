5bf517ec8048698a411fe073da5ac234eb3138d7
Ghulam Subbhani <gsmalik030@gmail.com>
2023-03-08 13:32:17 +0200

    Created Rockets page

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import fetchRockets from '../redux/rockets/Api';
+import Rocket from '../components/Rocket';
 
-const Rockets = () => (
-  <div className="Rockets-container" />
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch =useDispatch();
+  useEffect(()=>{
+    dispatch(fetchRockets())
+    },[]);
+    const newRockets = rockets.data;
+    const cards = newRockets.length > 0 && newRockets.map((rocket) => (
+      <RocketCard
+        key={rocket.id}
+        id={rocket.id}
+        name={rocket.name}
+        image={rocket.image}
+        desc={rocket.desc}
+        reserved={rocket.reserved}
+      />
+    ));
+  return(
+    <div className="rocket-container">
+    {cards}
+  </div>
+  );
+};
 
 export default Rockets;
---
+++ src/components/Rocket.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rockets';
+const Rocket = ({id, name, image, reserved, desc}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Rocket
