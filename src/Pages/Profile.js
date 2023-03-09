4d4a86d5918d79bc02bec034f27a73b05e59d633
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-03-09 12:11:18 +0300

    Setup Profile.js

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import MyRocketsProfile from '../component/rockets-profile';
+import MissionsProfile from '../component/missionsProfile';
 
-const Profile = () => (
-  <div className="Profile-container" />
-);
+const Profile = () => {
+  const myRockets = useSelector((state) => state.rockets);
+  const myReserveRockets = myRockets.rocketsData.filter((rocket) => rocket.reserved);
+
+  return (
+    <>
+
+      <div className="Profile-container">
+        <div className="rockets-profile">
+          <section className="my-rockets-sec">
+            <h2> My Rockets </h2>
+            <div className="box-border">
+              {myReserveRockets.length > 0 ? myReserveRockets.map((rocket) => (
+                <MyRocketsProfile
+                  key={rocket.id}
+                  rocketName={rocket.name}
+                  reserved={!!rocket.reserved}
+                  id={rocket.id}
+                  URL={rocket.wikipedia}
+                />
+              )) : <p>You don't have any reserved rockets</p>}
+            </div>
+          </section>
+        </div>
+        <MissionsProfile />
+      </div>
+    </>
+  );
+};
 
 export default Profile;
