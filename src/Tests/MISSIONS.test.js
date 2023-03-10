a4887587df6bb0456396d22ff54ea3741892a59f
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-03-10 11:20:08 +0300

    Make final changes

--- src/Tests/MISSIONS.test.js
+++ src/Tests/MISSIONS.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
-import Missions from '../Pages/MISSIONS';
+import Missions from '../Pages/Missions';
 import store from '../redux/configureStore';
 
 it('renders correctly', () => {
